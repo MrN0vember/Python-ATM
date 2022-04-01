b789a63dda659911c38a71da845eed37b0737287
James <j.a.pratt@outlook.com>
2022-04-01 11:34:51 +0100

    BankAccount Class Created

---
+++ ATM.py
@@ -0,0 +1,9 @@
+class BankAccount:
+    def __init__(self,account_number,customer_name,pin,balance = 0):
+        self.account_number = account_number
+        self.customer_name = customer_name
+        self.pin = pin
+        self.balance = balance
+
+    
+    
