4f778957e2fda139e23d5e4d8887e2dd17f5d320
James <j.a.pratt@outlook.com>
2022-04-02 17:19:07 +0100

    clear terminal after account creation and user interface added

--- ATM.py
+++ ATM.py
@@ -1,4 +1,4 @@
-import random
+import random, sys, os
 
 class Account:
     def __init__(self,account_number,customer_name,pin,balance = 0):
@@ -16,23 +16,19 @@ class Account:
         else:
             return False
 
-    def account_details(self):
-        print(f'Account Number: {self.account_number}')
-        print(f'Account Holder: {self.customer_name}')
-
     def check_balance(self):
-        return f"Your current balance is £ {self.balance}"
+        print(f"Your current balance is £ {self.balance}")
 
     def deposit(self,ammount):
         self.balance += ammount
-        return f"£{ammount} has been added to your account"
+        print(f"£{ammount} has been added to your account")
 
     def withdraw(self,ammount):
         if ammount > self.balance:
-            return f"Insufficient Funds, your balance is currently £{self.balance}" 
+            print(f"Insufficient Funds, your balance is currently £{self.balance}") 
         else:
             self.balance -= ammount
-            return f"£{ammount} has been withdrawn from your account"
+            print(f"£{ammount} has been withdrawn from your account")
 
         
 #Account Creation
@@ -67,36 +63,43 @@ accounts.append(account)
 print(f"Hello {customer_name}")
 print(f"Your account number is: {account_number}")
 print(f"Your new PIN is {pin}")
-    
+input(f"Pres enter to continue: ")
 
+os.system('cls' if os.name == 'nt' else 'clear')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-        
+#ATM interface
+def main():
+    while True:
+        print(f"Welcome to the Bank of Python.")
+        pin = int(input(f"Please enter your PIN: "))
+        while account.pin_verification(pin) != True:
+            pin =int(input(f"Incorrect PIN please try again: "))
             
-
+        while True:
+            print(f"Welcome {customer_name}, Account Number: {account_number} what would you like to do today?")
+            print("1: Check Balance")
+            print("2: Deposit Cash")
+            print("3: Withdaw Cash")
+            print("4: Exit")
             
+            user_choice = int(input(f"Please enter a numerical selection: "))
 
+            if user_choice == 1:
+                account.check_balance()
+               
+            elif user_choice == 2:
+                ammount = float(input("Please enter ammount you wish to deposit:"))
+                account.deposit(ammount)
+                
+            elif user_choice == 3:
+                ammount = float(input("Please enter ammount you wish to withdraw:"))
+                account.withdraw(ammount)
+                
+            elif user_choice == 4:
+                print("Thanks for choosing the Bank of Python")
+                exit()
 
-    
+            else:
+                print("Invalid Choice! Please select from 1,2,3,4 ")
+
+main()
