b0c069eea5387fe1df3d287f88071cd83fe916c8
James <j.a.pratt@outlook.com>
2022-04-02 14:02:47 +0100

    account generation added, class names updated

--- ATM.py
+++ ATM.py
@@ -1,9 +1,14 @@
-class BankAccount:
+import random
+
+class Account:
     def __init__(self,account_number,customer_name,pin,balance = 0):
         self.account_number = account_number
         self.customer_name = customer_name
         self.pin = pin
         self.balance = balance
+    
+    def __repr__(self):
+        return f'Account("{self.account_number}","{self.customer_name}",{self.pin},{self.balance})'
 
     def pin_verification(self,pin):
         if self.pin == pin:
@@ -16,20 +21,75 @@ class BankAccount:
         print(f'Account Holder: {self.customer_name}')
 
     def check_balance(self):
-        return self.balance
+        return f"Your current balance is £ {self.balance}"
 
     def deposit(self,ammount):
         self.balance += ammount
+        return f"£{ammount} has been added to your account"
 
     def withdraw(self,ammount):
-            self.balance += ammount
+        if ammount > self.balance:
+            return f"Insufficient Funds, your balance is currently £{self.balance}" 
+        else:
+            self.balance -= ammount
+            return f"£{ammount} has been withdrawn from your account"
+
+        
+#Account Creation
+accounts = []
+#account number generator
+def account_number_gen():
+    random_nums = []
+    for i in range(10):
+        n = random.randint(0,9)
+        random_nums.append(n)
+        account_num = int("".join(map(str,random_nums)))
+    return account_num
+account_number = account_number_gen()
+
+#PIN Generator 
+def pin_number_gen():
+    random_nums = []
+    for i in range(4):
+        n = random.randint(0,9)
+        random_nums.append(n)
+        pin_num = int("".join(map(str,random_nums)))
+    return pin_num
+pin = pin_number_gen()
+
+def get_customer_name():
+    name = input("Please enter your name: ")
+    return name
+customer_name = get_customer_name()
+
+account = Account(account_number,customer_name,pin,0)
+accounts.append(account)
+print(f"Hello {customer_name}")
+print(f"Your account number is: {account_number}")
+print(f"Your new PIN is {pin}")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
-#Account Creation
+    
 
-Accounts = {}
 
-def account_number_gen()
 
 
 
